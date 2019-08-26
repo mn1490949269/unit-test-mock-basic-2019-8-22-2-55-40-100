69110df70d7d456af18ae2a10fdbef2bad75eb89
mn <1490949269@qq.com>
2019-08-26 18:01:19 +0800

    mockito

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -3,7 +3,9 @@ package cashregister;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.mockito.internal.util.concurrent.DetachedThreadLocal.Cleaner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -28,6 +30,7 @@ public class CashRegisterTest {
 
 
     @Test
+    //@Disabled
     public void should_print_the_real_purchase_when_call_process() {
         //given
     	Item[] items = {new Item("food", 3) };
@@ -54,8 +57,14 @@ public class CashRegisterTest {
     @Test
     public void should_verify_with_process_call_with_mockito() {
         //given
+    	Printer printer = Mockito.mock(Printer.class);
+    	Purchase purchase = Mockito.mock(Purchase.class);
+    	Mockito.when(purchase.asString()).thenReturn("food\t3.0\n");
         //when
+    	CashRegister cashRegister = new CashRegister(printer);
+    	cashRegister.process(purchase);
         //then
+    	Mockito.verify(printer).print("food\t3.0\n");
     }
 
 }
